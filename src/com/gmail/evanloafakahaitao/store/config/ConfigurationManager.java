843b1eada426bec862a529c2167c9717cabdb18d
Ivan Karavai <evanloafakahaitao@gmail.com>
2022-04-26 14:03:29 +0300

    Add private constructor to configuration manager

--- src/com/gmail/evanloafakahaitao/store/config/ConfigurationManager.java
+++ src/com/gmail/evanloafakahaitao/store/config/ConfigurationManager.java
@@ -10,6 +10,8 @@ public class ConfigurationManager {
 
     private static final String BUNDLE_NAME = "config";
 
+    private ConfigurationManager() {}
+
     public static ConfigurationManager getInstance() {
         if (instance == null) {
             instance = new ConfigurationManager();

7e29b2e7e1726147ed4c5b7619b6d66f20952f51
Ivan Karavai <evanloafakahaitao@gmail.com>
2022-04-28 20:02:25 +0300

    Add logging filter

--- src/com/gmail/evanloafakahaitao/store/servlets/filter/LogFilter.java
+++ src/com/gmail/evanloafakahaitao/store/servlets/filter/LogFilter.java
@@ -1,4 +1,32 @@
 package com.gmail.evanloafakahaitao.store.servlets.filter;
 
-public class LogFilter {
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.time.LocalDateTime;
+
+public class LogFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("Logging Filter initialized");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        System.out.printf(
+                "#LOG_INFO %s %s URL = %s%s%n",
+                LocalDateTime.now(),
+                request.getHeader("User-Agent"),
+                request.getRequestURL(),
+                (request.getQueryString() == null) ? "" : "?" + request.getQueryString()
+        );
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Logging Filter destroyed");
+    }
 }

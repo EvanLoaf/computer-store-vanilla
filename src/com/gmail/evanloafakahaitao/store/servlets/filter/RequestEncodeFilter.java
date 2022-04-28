4598d6acf8591bf11de24a292ace57e0ba2947e8
Ivan Karavai <evanloafakahaitao@gmail.com>
2022-04-28 20:07:12 +0300

    Add request encode filter

--- src/com/gmail/evanloafakahaitao/store/servlets/filter/RequestEncodeFilter.java
+++ src/com/gmail/evanloafakahaitao/store/servlets/filter/RequestEncodeFilter.java
@@ -1,4 +1,24 @@
 package com.gmail.evanloafakahaitao.store.servlets.filter;
 
-public class RequestEncodeFilter {
+import javax.servlet.*;
+import java.io.IOException;
+
+public class RequestEncodeFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("Request Encode Filter initialized");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        servletRequest.setCharacterEncoding("UTF-8");
+        servletResponse.setContentType("text/html; charset=UTF-8");
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("Request Encode Filter destroyed");
+    }
 }

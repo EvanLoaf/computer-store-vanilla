17cdef4b47216e68bfb1dc4d2070c3d5ed666880
Ivan Karavai <evanloafakahaitao@gmail.com>
2022-04-28 08:50:23 +0300

    Add XML-related properties

--- src/com/gmail/evanloafakahaitao/store/config/properties/FileProperties.java
+++ src/com/gmail/evanloafakahaitao/store/config/properties/FileProperties.java
@@ -1,4 +1,7 @@
 package com.gmail.evanloafakahaitao.store.config.properties;
 
 public interface FileProperties {
+
+    String XML_FILE_PATH = "xml.file.path";
+    String SCHEMA_FILE_PATH = "schema.file.path";
 }

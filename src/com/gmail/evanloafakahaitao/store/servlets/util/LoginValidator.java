60f6d556f79e7722ee37ef13d4aac17bbc016da8
Ivan Karavai <evanloafakahaitao@gmail.com>
2022-04-28 20:18:04 +0300

    Add login validator

--- src/com/gmail/evanloafakahaitao/store/servlets/util/LoginValidator.java
+++ src/com/gmail/evanloafakahaitao/store/servlets/util/LoginValidator.java
@@ -1,4 +1,23 @@
 package com.gmail.evanloafakahaitao.store.servlets.util;
 
+import com.gmail.evanloafakahaitao.store.dao.model.User;
+import com.gmail.evanloafakahaitao.store.services.UserService;
+import com.gmail.evanloafakahaitao.store.services.impl.UserServiceImpl;
+
 public class LoginValidator {
+
+    private UserService userService = new UserServiceImpl();
+
+    public boolean validate(String email, String password) {
+        if (email != null && !email.equals("") && password != null && !password.equals("")) {
+            User user = userService.findByEmail(email.trim());
+            if (user != null) {
+                return user.getPassword().equals(password.trim());
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
 }

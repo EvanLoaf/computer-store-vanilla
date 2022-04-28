cc705e94a2226f9cb89cfdeb5f69f2f9c842ef64
Ivan Karavai <evanloafakahaitao@gmail.com>
2022-04-28 20:41:04 +0300

    Add items command implementation

--- src/com/gmail/evanloafakahaitao/store/servlets/command/impl/ItemsCommand.java
+++ src/com/gmail/evanloafakahaitao/store/servlets/command/impl/ItemsCommand.java
@@ -1,4 +1,30 @@
 package com.gmail.evanloafakahaitao.store.servlets.command.impl;
 
-public class ItemsCommand {
+import com.gmail.evanloafakahaitao.store.config.ConfigurationManager;
+import com.gmail.evanloafakahaitao.store.config.properties.PageProperties;
+import com.gmail.evanloafakahaitao.store.dao.model.Item;
+import com.gmail.evanloafakahaitao.store.services.ItemService;
+import com.gmail.evanloafakahaitao.store.services.impl.ItemServiceImpl;
+import com.gmail.evanloafakahaitao.store.servlets.command.Command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class ItemsCommand implements Command {
+
+    private ItemService itemService = new ItemServiceImpl();
+    private ConfigurationManager configurationManager = ConfigurationManager.getInstance();
+
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        List<Item> items = itemService.findAll();
+        if (items != null) {
+            request.setAttribute("items", items);
+        } else {
+            request.setAttribute("error", "Error retrieving items");
+        }
+        return configurationManager.getProperty(PageProperties.ITEMS_PAGE_PATH);
+    }
 }

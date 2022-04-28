0d3ae720d61bc7a9bc324eddc9b08a6a65b61ef2
Ivan Karavai <evanloafakahaitao@gmail.com>
2022-04-28 10:05:39 +0300

    Add XML service implementation

--- src/com/gmail/evanloafakahaitao/store/services/impl/XmlServiceImpl.java
+++ src/com/gmail/evanloafakahaitao/store/services/impl/XmlServiceImpl.java
@@ -1,4 +1,33 @@
 package com.gmail.evanloafakahaitao.store.services.impl;
 
-public class XmlServiceImpl {
+import com.gmail.evanloafakahaitao.store.dao.XmlDao;
+import com.gmail.evanloafakahaitao.store.dao.impl.XmlDaoImpl;
+import com.gmail.evanloafakahaitao.store.services.XmlService;
+import com.gmail.evanloafakahaitao.store.services.model.ItemXmlBinding;
+import com.gmail.evanloafakahaitao.store.services.model.ItemsXmlBinding;
+import com.gmail.evanloafakahaitao.store.services.util.XmlParser;
+import com.gmail.evanloafakahaitao.store.services.util.XmlValidator;
+
+import java.io.File;
+import java.util.List;
+
+public class XmlServiceImpl implements XmlService {
+
+    private XmlDao xmlDao = new XmlDaoImpl();
+    private XmlValidator xmlValidator = new XmlValidator();
+    private XmlParser xmlParser = new XmlParser();
+
+    @Override
+    public List<ItemXmlBinding> getItems(String filePath, String schemaPath) {
+        ItemsXmlBinding items = new ItemsXmlBinding();
+        File xml = xmlDao.getFile(filePath);
+        File schema = xmlDao.getFile(schemaPath);
+        boolean isValid = xmlValidator.validate(xml, schema);
+        System.out.printf("XML file is valid : %s%n", isValid);
+        if (isValid) {
+            System.out.println("Parsing XML file...");
+            items = xmlParser.unmarshal(xml);
+        }
+        return items.getItems();
+    }
 }

39f44e50e0a2c10f984383e72ae6f5fd96d0f78c
Ivan Karavai <evanloafakahaitao@gmail.com>
2022-04-28 09:40:53 +0300

    Add XML validator util

--- src/com/gmail/evanloafakahaitao/store/services/util/XmlValidator.java
+++ src/com/gmail/evanloafakahaitao/store/services/util/XmlValidator.java
@@ -1,4 +1,32 @@
 package com.gmail.evanloafakahaitao.store.services.util;
 
+import org.xml.sax.SAXException;
+
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.File;
+import java.io.IOException;
+
 public class XmlValidator {
+
+    public boolean validate(File xmlFile, File schemaFile) {
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        try {
+            Schema schema = schemaFactory.newSchema(schemaFile);
+            Validator validator = schema.newValidator();
+            validator.validate(new StreamSource(xmlFile));
+        } catch (SAXException e) {
+            System.out.println("XML validation against schema failed");
+            e.printStackTrace();
+            return false;
+        } catch (IOException e) {
+            System.out.println("Error accessing XML file for validation");
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 }

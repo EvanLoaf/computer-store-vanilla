2efdc1ac9a49e508a8fc2ad3bc14366c6b720710
Ivan Karavai <evanloafakahaitao@gmail.com>
2022-04-29 16:26:43 +0300

    Add user validator

---
+++ src/com/gmail/evanloafakahaitao/store/servlets/util/UserValidator.java
@@ -0,0 +1,47 @@
+package com.gmail.evanloafakahaitao.store.servlets.util;
+
+import com.gmail.evanloafakahaitao.store.dao.model.User;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class UserValidator {
+
+    public boolean validate(User user) {
+        if (user.getEmail() != null) {
+            Pattern emailPattern = Pattern.compile("^(?=[\\da-zA-Z@\\.\\_]{5,30}$)\\S+@\\S+\\.\\S+");
+            Matcher emailMatcher = emailPattern.matcher(user.getEmail());
+            if (!emailMatcher.find()) {
+                return false;
+            }
+        }
+        if (user.getPassword() != null && user.getPassword().length() > 30) {
+            return false;
+        }
+        if (user.getFirstName() != null) {
+            Pattern namePattern = Pattern.compile("^[a-zA-Z]{1,25}$");
+            Matcher fnMatcher = namePattern.matcher(user.getFirstName());
+            if (!fnMatcher.find()) {
+                return false;
+            }
+        }
+        if (user.getLastName() != null) {
+            Pattern namePattern = Pattern.compile("^[a-zA-Z]{1,25}$");
+            Matcher lnMatcher = namePattern.matcher(user.getLastName());
+            if (!lnMatcher.find()) {
+                return false;
+            }
+        }
+        if (user.getPhoneNumber() != null) {
+            Pattern phonePattern = Pattern.compile("^[\\+\\d][\\-\\da-zA-Z]{2,14}$");
+            Matcher phoneMatcher = phonePattern.matcher(user.getPhoneNumber());
+            if (!phoneMatcher.find()) {
+                return false;
+            }
+        }
+        if (user.getAdditionalInfo() != null && user.getAdditionalInfo().length() > 100) {
+            return false;
+        }
+        return true;
+    }
+}
